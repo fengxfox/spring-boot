9801ac6e181c617b58360a37f45bf0f4792d112f
feng <feng@127.0.0.1>
2018-01-21 19:20:35 +0800

    *注释

--- src/main/java/com/kfit/Application.java
+++ src/main/java/com/kfit/Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	//2
-	//11234
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
